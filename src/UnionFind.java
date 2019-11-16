d37f2afc79918f176505c0d6494b79f385fd621f
James Richardson <james.richardson@aggiemail.usu.edu>
2019-11-15 23:37:22 -0700

    I added the code for Union and Find.

--- src/UnionFind.java
+++ src/UnionFind.java
@@ -5,5 +5,36 @@
  */
 
 public class UnionFind {
+    Integer[] list;
+
+    UnionFind(Integer[] list) {
+        this.list = list;
+    }
+
+    public void union(Integer one, Integer two) {
+        Integer root1 = find(one);
+        Integer root2 = find(two);
+
+        if(list[root1] < list[root2]) {
+            list[root1] += list[root2];
+            list[root2] = root1;
+        }
+        else {
+            list[root2] += list[root1];
+            list[root1] = root2;
+        }
+
+    }
+
+    public Integer find(Integer toFind) {
+        if(list[toFind] < 0) {
+            return toFind;
+        }
+        else {
+            Integer root = find(list[toFind]);
+            list[toFind] = root;
+            return root;
+        }
+    }
 
 }
