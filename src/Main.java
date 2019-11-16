43e742f30061d87fa89185ba3b9a1a9302e2a894
Nathan <nathan.johnson999@aggiemail.usu.edu>
2019-11-15 22:35:58 -0700

    added text fields

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -15,8 +16,9 @@ public class Main extends Application {
 
     public void start(Stage stage){
         BorderPane pane = new BorderPane();
-
-
+        
+        Text player1turn = new Text("Player 1's turn (red)");
+        Text player2turn = new Text("Player 2's turn (blue)");
 
 
 
