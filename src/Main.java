790332e6ba4396ea9f022fd0ed6afa0884a6bab2
Nathan <nathan.johnson999@aggiemail.usu.edu>
2019-11-15 21:39:47 -0700

    added basic javaFX outline as well as a blank union find file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
-public class Main {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+    public void start(Stage stage){
+        BorderPane pane = new BorderPane();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        Scene scene = new Scene(pane);
+        stage.setScene(scene);
+        stage.setMaximized(true);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
     }
 }
